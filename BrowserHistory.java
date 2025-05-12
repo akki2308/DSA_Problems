30ce62d34b8dd51b65119d4b4d25af93d6d39804
Akki <chauhanakshit61@gmail.com>
2025-05-12 21:42:19 +0530

    [Akshit] ADD: Browser History

---
+++ BrowserHistory.java
@@ -0,0 +1,38 @@
+class BrowserHistory {
+
+    private class Node{
+        Node prev,next;
+        String url;
+        Node(String url){
+            this.url=url;
+        }
+    }
+
+    private Node currentNode;
+    public BrowserHistory(String homepage) {
+        currentNode =new Node(homepage);
+    }
+
+    public void visit(String url) {
+        Node newNode = new Node(url);
+        currentNode.next=null;
+        newNode.prev= currentNode;
+        currentNode.next=newNode;
+        currentNode =newNode;
+    }
+
+    public String back(int steps) {
+        while(steps-- > 0 && currentNode.prev!=null){
+            currentNode = currentNode.prev;
+        }
+        return currentNode.url;
+    }
+
+    public String forward(int steps) {
+        while(steps-- > 0 && currentNode.next!=null){
+            currentNode = currentNode.next;
+        }
+        return currentNode.url;
+    }
+}
+
